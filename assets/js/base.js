db37a9a9f4c17581566cb10955013f425e7150d3
Pieter Michels <pieter@noort.be>
2011-09-21 11:20:16 +0200

    fast refactor

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,61 +1,117 @@
 $(document).ready(function(){
-  
-   initGenerator(); 
+
+    window.soc_gen = new SocialGenerator();
 
 });
 
-var twitter_link;
-var fb_link;
-var hotmail_link;
-var gmail_link;
-                
-var copy_twitte;
-var copy_mail;
-var subject_mail;
-var share_link;
+function SocialGenerator() {
+  this.init();
+}
 
-function initGenerator() {
-  $("#copy-twitter").keyup(function(e) {
-    if($("#copy-twitter").val().length > 140) 
-      $("#copy-twitter").parent().parent().addClass("error");
-  });
+SocialGenerator.prototype = {
+  twitter_link: null,
+  twitter_link: null,
+  fb_link: null, 
+  hotmail_link: null,
+  gmail_link: null,
+              
+  copy_twitter: null,
+  copy_mail: null,
+  subject_mail: null,
+  share_link: null,
 
-  $("#generator").submit(function(e) {
-    e.preventDefault();
+  validation_timeout: null,
 
-    twitter_link = $("#url-twitter").val();
-    fb_link = $("#url-fb").val();
-    hotmail_link = $("#url-hotmail").val();
-    gmail_link = $("#url-gmail").val();
+  init:
+    function() {
+      var ref = this;
 
-    copy_twitter = $("#copy-twitter").val();
-    copy_mail = $("#copy-mail").val();
-    subject_mail = $("#subject-mail").val();
-    share_link = $("#share-link").val();
+      this.listen_for_max_chars("#copy-twitter", 140);
 
-    if(valid_form()) {
-      $("#link-twitter").val(replace_arr(twitter_link, [{key: "text", val: copy_twitter}]));
-      $("#link-fb").val(replace_arr(fb_link, [{key: "link", val: share_link}, {key: "title", val: ""}]));
-      $("#link-hotmail").val(replace_arr(hotmail_link, [{key: "text", val: copy_mail}, {key: "subject", val: subject_mail}, {key: "link", val: share_link}]));
-      $("#link-gmail").val(replace_arr(gmail_link, [{key: "text", val: copy_mail}, {key: "subject", val: subject_mail}, {key: "link", val: share_link}]));
+      $("#generator").submit(function(e) {
+        e.preventDefault();
+
+        ref.generate();
+            
+        return false;
+      });
+    },
+
+  generate:
+    function() {
+      this.store_vars();
+
+      if(this.validate_form()) {
+        $("#link-twitter").val(this.replace_arr(this.twitter_link, [{key: "text", val: this.copy_twitter}]));
+        $("#link-fb").val(this.replace_arr(this.fb_link, [ {key: "link", val: this.share_link}, 
+                                                            {key: "title", val: ""}]));
+
+        var mail_arr = [  {key: "text", val: this.copy_mail}, 
+                          {key: "subject", val: this.subject_mail}, 
+                          {key: "link", val: this.share_link}];
+
+        $("#link-hotmail").val(this.replace_arr(this.hotmail_link, mail_arr));
+        $("#link-gmail").val(this.replace_arr(this.gmail_link, mail_arr));
+      } else {
+        this.validation_timeout = setTimeout(this.reset_validation, 5000);
+      }
+    },
+
+  store_vars:
+    function() {
+      this.twitter_link = $("#url-twitter").val();
+      this.fb_link = $("#url-fb").val();
+      this.hotmail_link = $("#url-hotmail").val();
+      this.gmail_link = $("#url-gmail").val();
+
+      this.copy_twitter = $("#copy-twitter").val();
+      this.copy_mail = $("#copy-mail").val();
+      this.subject_mail = $("#subject-mail").val();
+      this.share_link = $("#share-link").val();
+    },
+
+  validate_form:
+    function() {
+      var valid = true;
+
+      this.reset_validation();
+
+      if(this.copy_twitter.length > 140) {
+        $("#copy-twitter").parent().parent().addClass("error");
+
+        valid = false;
+      }
+
+      if(!/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(this.share_link)) {
+        $("#share-link").parent().addClass("error");
+
+        valid = false;
+      }
+
+      return valid;
+    },
+
+  reset_validation:
+    function() {
+      clearTimeout(this.validation_timeout);
+
+      $("#copy-twitter").parent().parent().removeClass("error");
+      $("#share-link").parent().removeClass("error");
+    },
+
+  replace_arr:
+    function (value, list) {
+      for(var i = 0; i < list.length; i++)
+        value = value.replace(new RegExp("{" + list[i].key + "}"), encodeURIComponent(list[i].val));
+
+      return value;
+    },
+
+  listen_for_max_chars:
+    function (field, max) {
+      $(field).keyup(function(e) {
+        if($(this).val().length > max) 
+          $(this).parent().parent().addClass("error");
+      });
     }
-
-    return false;
-  });
-}
-
-function valid_form() {
-  var valid = true;
-
-  if(copy_twitter.length > 140)
-    $("#copy-twitter").parent().parent().addClass("error");
-
-  return valid;
-}
-
-function replace_arr(value, list) {
-  for(var i = 0; i < list.length; i++)
-    value = value.replace(new RegExp("{" + list[i].key + "}"), encodeURIComponent(list[i].val));
-
-  return value;
-}
+};
